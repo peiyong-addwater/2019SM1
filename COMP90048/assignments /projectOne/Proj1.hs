2eb393ff6cb27b70983c22ed4a39585ef741a4b2
peiyongwang <3014210022@tju.edu.cn>
2019-04-06 18:24:42 +1100

    updates

---
+++ COMP90048/assignments /projectOne/Proj1.hs
@@ -0,0 +1,9 @@
+--  File     : Proj1.hs
+--  ID       : 955986
+--  Author   : Peiyong Wang
+--  LMS ID   : peiyongw 
+--  Purpose  : This is a program that plays the game of "Musician", which implements the performer and the composer part of the game
+
+module Proj1 (Pitch, toPitch, feedback, GameState, initialGuess, nextGuess) where
+
+
