9a2eafe6d467772dea247a84d9c965ca12be43ad
peiyongwang <3014210022@tju.edu.cn>
2019-04-19 19:34:31 +1000

    updates

--- COMP90048/assignments /assignmentTwo/assignment2.pl
+++ COMP90048/assignments /assignmentTwo/assignment2.pl
@@ -14,5 +14,34 @@ samelength([E1,E2|Tail]) :-
     length(E1,N),
     length(E2,N),
     samelength([E1|Tail]).
-    
+
 % Question Two: interleave(Ls, L)
+getHead([], Yrest,[],Yrest).
+getHead([[Ele|Xs]|XXs], [Ele|Ys], [Xs|XXrest],Yrest):-
+    getHead(XXs,Ys,XXrest,Yrest).
+interleave([],[]).
+interleave([[]|Xs],[]):-
+    interleave(Xs,[]).
+interleave([X|Xs], [Y|Ys]):-
+    getHead([X|Xs],[Y|Ys],Xrest,Yrest),
+    interleave(Xrest,Yrest).
+
+% Question Three: partial eval(Expr0, Var, Val, Expr)
+partial_eval(+(A,B),Var,Val,E):-
+    partial_eval(A,Var,Val,C),partial_eval(B,Var,Val,D),((number(C),number(D),E is +(C,D));(\+ (number(C),number(D)),E = +(C,D))).
+partial_eval(-(A,B),Var,Val,E):-
+    partial_eval(A,Var,Val,C),partial_eval(B,Var,Val,D),((number(C),number(D),E is -(C,D));(\+ (number(C),number(D)),E = -(C,D))).
+partial_eval(*(A,B),Var,Val,E):-
+    partial_eval(A,Var,Val,C),partial_eval(B,Var,Val,D),((number(C),number(D),E is *(C,D));(\+ (number(C),number(D)),E = *(C,D))).
+partial_eval(/(A,B),Var,Val,E):-
+    partial_eval(A,Var,Val,C),partial_eval(B,Var,Val,D),((number(C),number(D),E is /(C,D));(\+ (number(C),number(D)),E = /(C,D))).
+partial_eval(//(A,B),Var,Val,E):-
+    partial_eval(A,Var,Val,C),partial_eval(B,Var,Val,D),((number(C),number(D),E is //(C,D));(\+ (number(C),number(D)),E = //(C,D))).
+partial_eval(Expr0,Var,Val,Expr):-
+    ground(Expr0),
+    ground(Var),
+    ground(Val),
+    \+ground(Expr),
+    atom(Var),
+    number(Val),
+    ((Expr0 = Var,Val = Expr); (Expr0 \= Var,Expr0 = Expr, (atom(Expr0); number(Expr0)))).
